ced1425a37a2a08dacedc3568a921f00dedb8249
Anton Romanuyk <anton.romanuyk1@gmail.com>
2025-11-02 17:04:29 +0000

    Add router for comment

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 
-from app.routers import user, post, auth, like
+
+from app.routers import user, post, auth, like, comment
 
 app = FastAPI()
 origins = ["*"]
@@ -19,6 +20,7 @@ app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(auth.router)
 app.include_router(like.router)
+app.include_router(comment.router)
 
 #app.mount("/uploads/avatars", StaticFiles(directory="uploads/avatars"), name="avatars")
 #app.mount("/uploads/post_files", StaticFiles(directory="uploads/post_files"), name="post_files")
